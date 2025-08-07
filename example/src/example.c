fde4cd1798f1689fb9adf6c9521bb71b78ddb894
gundlachj <gundlachj@msoe.edu>
2025-08-06 20:55:21 -0400

    added start of node struct

--- example/src/example.c
+++ example/src/example.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "graph/node.h"
 
 int main(int argc, char const *argv[]) {
   printf("Hello World!\n");
+
+  node_t *node = malloc(sizeof(node_t));
+  node->key = 1;
+  printf("%d\n", node->key);
   return 0;
 }
---
+++ graph/include/graph/node.h
@@ -0,0 +1,8 @@
+#ifndef NODE_H
+#define NODE_H
+
+typedef struct {
+  int key;
+} node_t;
+
+#endif /* end of include guard: NODE_H */
