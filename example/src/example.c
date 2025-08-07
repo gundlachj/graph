681eb5c1bb5bba41b624ebe1de1174d9ddf832ea
gundlachj <gundlachj@msoe.edu>
2025-08-06 21:53:29 -0400

    added node printing

--- example/src/example.c
+++ example/src/example.c
@@ -3,9 +3,21 @@
 
 #include "graph/node.h"
 
-int main(int argc, char const *argv[]) {
-  printf("Hello World!\n");
 
+void printNode(const node_t *node) {
+  printf("Node: ");
+  printf("[Key: %d] ", node->key);
+  printf("[Edges: ");
+  for (int i = 0; i < node->degree; i++) {
+    printf("%d", node->edges[i]->key);
+    if (i != node->degree - 1) {
+      printf(", ");
+    }
+  }
+  printf("]\n");
+}
+
+int main(int argc, char const *argv[]) {
   node_t *n1 = malloc(sizeof(node_t));
   node_t *n2 = malloc(sizeof(node_t));
   node_t *n3 = malloc(sizeof(node_t));
@@ -19,19 +31,17 @@ int main(int argc, char const *argv[]) {
   addEdge(n1, n2);
   addEdge(n1, n3);
 
-  for (int i = 0; i < n1->degree; i++) {
-    printf("%d\n", n1->edges[i]->key);
-  }
+  printNode(n1);
 
   printf("\n");
 
   int err = disconnectEdge(n1, n4);
-  printf("Error value: %d\n", err);
-
-  for (int i = 0; i < n1->degree; i++) {
-    printf("%d\n", n1->edges[i]->key);
+  if (err == 1) {
+    printf("Failed to disconnect node with key (%d)\n", n4->key);
+  } else {
+    printf("Disconnected node with key (%d)\n", n4->key);
+    printNode(n1);
   }
-
 
   return 0;
 }
